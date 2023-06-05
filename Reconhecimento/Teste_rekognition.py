9dffa975e5ab43358e8a71ae889a4c7bc538e214
João Felipe Lobato <89958345+Imperador-Joao@users.noreply.github.com>
2023-06-05 12:50:14 -0300

    Add files via upload

--- Reconhecimento/Teste_rekognition.py
+++ Reconhecimento/Teste_rekognition.py
@@ -1,8 +1,12 @@
 from os import system
 import boto3
 import csv
+from googletrans import Translator
 
-
+def traduzir(texto, idioma_origem = 'en', idioma_destino = 'pt'):
+    tradutor = Translator()
+    traducao = tradutor.translate(texto, src = idioma_origem, dest = idioma_destino)
+    return traducao.text
 
 def tirar_foto(nome = 'foto_camera'):
     system(f"fswebcam --resolution 640x480 --skip 10 {nome}.jpg")
@@ -13,8 +17,15 @@ with open('Chaves acesso.csv','r') as credenciais:
     next(credenciais)
     chave,senha = list(csv.reader(credenciais))[0]
 
-nome_foto = 'eu'
-#tirar_foto(nome_foto)
+nome_foto = 'foto_teste'
+tirar_foto(nome_foto)
+
+resposta = input('A foto ficou boa?\t')
+
+while resposta.lower() != 'sim':
+    tirar_foto(nome_foto)
+    resposta = input('A foto ficou boa?\t') 
+
 
 nome_arquivo_foto = f'{nome_foto}.jpg'
 
@@ -27,7 +38,9 @@ cliente = boto3.client('rekognition',
 with open(nome_arquivo_foto,'rb') as imagem_fonte:
     bytes_fonte = imagem_fonte.read()
 
-resposta = cliente.detect_labels(Image = {'Bytes':bytes_fonte},MaxLabels = 10)
+retorno = cliente.detect_labels(Image = {'Bytes':bytes_fonte},MaxLabels = 5)
 
-for chave,valor in resposta.items():
-    print(f'{chave}\t {valor}\n')
+itens_detectados = retorno.get('Labels')
+imagens_confiabilidades = {traduzir(objeto.get('Name')):objeto.get('Confidence') for objeto in itens_detectados}
+
+print(imagens_confiabilidades)
