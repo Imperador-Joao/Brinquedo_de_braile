6019436965db528aa9b59d9b44e865f89a27e94e
João Felipe Lobato <89958345+Imperador-Joao@users.noreply.github.com>
2023-06-07 15:35:14 -0300

    Add files via upload

--- reconhecimento/detector.py
+++ reconhecimento/detector.py
@@ -2,19 +2,25 @@ from os import system
 import boto3
 
 
-def tirar_foto(nome = 'foto_camera'):
-    system(f"fswebcam --resolution 640x480 --skip 10 {nome}.jpg")
+def tirar_foto(nome = 'foto_camera',sistema_operacional = 'Linux'):
 
+    argumentos = {'Linux': f"fswebcam --resolution 640x480 --skip 10 {nome}.jpg",
+                  'Windows':f"CommandCam /filename {nome}.jpg /delay 500"}
+
+    argumento_sistema = argumentos.get(sistema_operacional)
+    system(argumento_sistema)
+
+    '''
     resposta = input('A foto ficou boa?\t')
 
     if resposta.lower() != 'sim':
         tirar_foto(nome)
-
-class Detector:
+    '''
+    
+class Detector_aws:
 
     def __init__(self,chave,senha):
-        self.chave = chave
-        self.senha = senha
+
         self.cliente = boto3.client('rekognition',
                        aws_access_key_id = chave,
                        aws_secret_access_key = senha,
